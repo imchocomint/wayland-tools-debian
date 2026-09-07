68bda18faf64659d2d98d8ad3ddf0ba72cd49c00
Trần Thiên Minh <118097966+imchocomint@users.noreply.github.com>
2026-09-07 17:42:19 +0700

    Patch awww build process

--- awww-build.sh
+++ awww-build.sh
@@ -1,1 +1,4 @@
+wget https://archlinux.org/packages/extra/x86_64/awww/download/ --content-disposition
+tar -xvf awww-*-x86_64.pkg.tar.zst -C ./awww usr
+rm -rf awww-*-x86_64.pkg.tar.zst
 sudo dpkg -b awww
